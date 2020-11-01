a2f987d679487f06e4fe8904a8b5ab788090a06e
Polomac <milan.polomac@gmail.com>
2020-11-01 19:08:33 +0100

    Minor fix

--- src/App.vue
+++ src/App.vue
@@ -16,25 +16,6 @@ export default {
     'app-header': appHeader,
     'app-footer': appFooter,
   },
-  metaInfo() {
-    return {
-      meta: [
-        // Twitter Card
-        { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:title', content: 'Air Traffic' },
-        { name: 'twitter:keywords', content: 'air, traffic, tracking, flights, airplane, real, time' },
-        { name: 'twitter:description', content: 'Air traffic, real time tracker' },
-        { name: 'twitter:image', content: process.env.NODE_ENV === 'production' ? 'https://air-traffic.netlify.app/static/air-traffic.jpg' : '' },
-        // Facebook OpenGraph
-        { property: 'og:title', content: 'Air Traffic' },
-        { property: 'og:site_name', content: 'Vue Example' },
-        { property: 'og:type', content: 'website' },
-        { property: 'og:keywords', content: 'air, traffic, tracking, flights, airplane, real, time' },
-        { property: 'og:image', content: process.env.NODE_ENV === 's' ? 'https://air-traffic.netlify.app/static/air-traffic.jpg' : '' },
-        { property: 'og:description', content: 'Air traffic, real time tracker' },
-      ],
-    };
-  },
 };
 </script>
 
